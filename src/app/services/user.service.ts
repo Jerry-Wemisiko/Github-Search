29220a041f8ab6d048a065df09fc6dc7efdf6810
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-07-24 23:22:53 +0300

    subscribed to user data from github api

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import {UInterface} from '../interfaces/user'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private user_url_search:string="https://api.github.com/users/{user}";
+  private user_url_search:string="https://api.github.com/users/";
  
   
   constructor(private http:HttpClient) { }
    
   //Multiple Users
-  getUsers(username:string){
+  getUsers(username:string):Observable<UInterface>{
     var outputUrl = this.user_url_search +username
-    console.log(this.http.get(outputUrl))
+    var data:Observable<UInterface> =this.http.get<UInterface>(outputUrl);
+    return data;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,22 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  private searchPattern:string =''
-
+  private searchPattern:string ="Jerry-Wemisiko";
+  private aboutUser:any;
   constructor(private userService:UserService) { }
 
   ngOnInit(): void {
     this.userService.getUsers(this.searchPattern)
+    .subscribe((results)=> {
+      this.aboutUser = results;
+      console.log(results);
+    });
   }
 
 }
